27bddc325062ab617d0ef957c5492fc828f6c319
mnghiap <phanminhnghia98@gmail.com>
2021-07-14 15:42:54 +0200

    fixed filter on variants

--- pm4pygpu/variants.py
+++ pm4pygpu/variants.py
@@ -4,32 +4,23 @@ import sys
 
 def get_variants_df(df):
 	vdf = df.groupby(Constants.TARGET_CASE_IDX).agg({Constants.TARGET_ACTIVITY_CODE: "collect", Constants.TARGET_EV_IDX: "count"}).reset_index()
+	vars_list = vdf[Constants.TARGET_ACTIVITY_CODE].to_arrow().to_pylist()
+	vars_list = list(map(lambda x: ",".join([str(e) for e in x]), vars_list))
+	vdf[Constants.TEMP_COLUMN_1] = vars_list
 	return vdf
 
 def filter_on_variants(df, allowed_variants):
 	activities = df[Constants.TARGET_ACTIVITY].cat.categories.to_arrow().to_pylist()
 	activities = {activities[i]: i for i in range(len(activities))}
-	list_tup_vars = []
-	for varstri in allowed_variants:
-		var = varstri.split(Constants.VARIANTS_SEP)
-		v1 = 0
-		v2 = 0
-		for i in range(len(var)):
-			v1 += activities[var[i]]
-			v2 += (len(var) + i + 1) * (activities[var[i]] + 1)
-		list_tup_vars.append("(" + Constants.TARGET_ACTIVITY_CODE + " == "+str(v1)+" and " +Constants.TARGET_VARIANT_NUMBER+" == "+str(v2) + ")")
-	this_query = " or ".join(list_tup_vars)
-	cdf = get_variants_df(df)
-	cdf = cdf.query(this_query)[Constants.TARGET_CASE_IDX]
-	return df[df[Constants.TARGET_CASE_IDX].isin(cdf)]
+	varstrings = list(map(lambda x: ",".join([str(activities[act]) for act in x.split(",")]), allowed_variants))
+	vdf = get_variants_df(df)
+	vdf = vdf[vdf[Constants.TEMP_COLUMN_1].isin(varstrings)]
+	return df[df[Constants.TARGET_CASE_IDX].isin(vdf[Constants.TARGET_CASE_IDX])]
 
 def get_variants(df, max_des_vars_num=sys.maxsize, return_list=False):
 	activities = df[Constants.TARGET_ACTIVITY].cat.categories.to_arrow().to_pylist()
 	activities = {i: activities[i] for i in range(len(activities))}
 	vdf = get_variants_df(df)
-	vars_list = vdf[Constants.TARGET_ACTIVITY_CODE].to_arrow().to_pylist()
-	vars_list = list(map(lambda x: ",".join([str(e) for e in x]), vars_list))
-	vdf[Constants.TEMP_COLUMN_1] = vars_list
 	vars_count = vdf[Constants.TEMP_COLUMN_1].value_counts()
 	if max_des_vars_num < sys.maxsize:
 		nlarg_vars = list(vars_count.nlargest(max_des_vars_num).to_pandas().to_dict().keys())
