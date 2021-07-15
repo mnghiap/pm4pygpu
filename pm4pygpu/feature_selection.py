835ff2a5135626b949af6b6b4d87dfddbed6124d
mnghiap <phanminhnghia98@gmail.com>
2021-07-16 01:42:22 +0200

    add select attribute paths

--- pm4pygpu/feature_selection.py
+++ pm4pygpu/feature_selection.py
@@ -44,3 +44,30 @@ def select_features(df, low_b_str=5, up_b_str=50):
 def get_automatic_features_df(df, low_b_str=5, up_b_str=50):
 	list_columns = select_features(df, low_b_str=low_b_str, up_b_str=up_b_str)
 	return get_features_df(df, list_columns)
+
+def select_attribute_paths(df, fea_df, att):
+	'''
+	For an attribute att and two values v1, v2, column value att@v1->v2=0 if no such directly-follow happens in the case, elsewhile = #occurences of the path.
+	Assumption: df is sorted by case and timestamp as in format.py
+	'''
+	df = df.copy()
+	df = df.sort_values(by = [Constants.TARGET_CASE_IDX, Constants.TARGET_TIMESTAMP])
+	vals = df[att].unique().to_arrow().to_pylist()
+	att_numeric = att + '_numeric'
+	df[att_numeric] = df[att].astype('category').cat.codes
+	keys = df[att_numeric].to_arrow().to_pylist()
+	values = df[att].to_arrow().to_pylist()
+	att_dict = {key: value for key, value in zip(keys, values)}
+	df[Constants.TEMP_COLUMN_2] = df[att_numeric]
+	df = df.groupby(Constants.TARGET_CASE_IDX).apply_grouped(paths_udf, incols = [Constants.TEMP_COLUMN_2], outcols= {Constants.TEMP_COLUMN_1: np.int32})
+	#df = df.query(Constants.TARGET_CASE_IDX + "==" + Constants.TARGET_PRE_CASE).groupby([Constants.TARGET_CASE_IDX, Constants.TEMP_COLUMN_1, Constants.TEMP_COLUMN_2]).count().reset_index()
+	for v1 in att_dict.keys():
+		for v2 in att_dict.keys():
+			ev_idxs = df.query(Constants.TEMP_COLUMN_1+"=="+str(v1)+" and "+Constants.TEMP_COLUMN_2+"=="+str(v2))[Constants.TARGET_EV_IDX].unique()
+			str_v1 = att_dict[v1].encode('ascii',errors='ignore').decode('ascii').replace(" ","")
+			str_v2 = att_dict[v2].encode('ascii',errors='ignore').decode('ascii').replace(" ","")
+			df[att+"@"+str_v1+"->"+str_v2] = df[Constants.TARGET_EV_IDX].isin(ev_idxs).astype("int")
+	df = df[[Constants.TARGET_CASE_IDX] + [att+"@"+v1+"->"+v2 for v1 in att_dict.values() for v2 in att_dict.values()]]
+	df = df.groupby(Constants.TARGET_CASE_IDX).sum().reset_index()
+	fea_df = fea_df.merge(df, on=[Constants.TARGET_CASE_IDX], how="left", suffixes=('','_y'))
+	return fea_df
--- pm4pygpu/variants.py
+++ pm4pygpu/variants.py
@@ -5,14 +5,14 @@ import sys
 def get_variants_df(df):
 	vdf = df.groupby(Constants.TARGET_CASE_IDX).agg({Constants.TARGET_ACTIVITY_CODE: "collect", Constants.TARGET_EV_IDX: "count"}).reset_index()
 	vars_list = vdf[Constants.TARGET_ACTIVITY_CODE].to_arrow().to_pylist()
-	vars_list = list(map(lambda x: ",".join([str(e) for e in x]), vars_list))
+	vars_list = list(map(lambda x: Constants.VARIANTS_SEP.join([str(e) for e in x]), vars_list))
 	vdf[Constants.TEMP_COLUMN_1] = vars_list
 	return vdf
 
 def filter_on_variants(df, allowed_variants):
 	activities = df[Constants.TARGET_ACTIVITY].cat.categories.to_arrow().to_pylist()
 	activities = {activities[i]: i for i in range(len(activities))}
-	varstrings = list(map(lambda x: ",".join([str(activities[act]) for act in x.split(",")]), allowed_variants))
+	varstrings = list(map(lambda x: Constants.VARIANTS_SEP.join([str(activities[act]) for act in x.split(Constants.VARIANTS_SEP)]), allowed_variants))
 	vdf = get_variants_df(df)
 	vdf = vdf[vdf[Constants.TEMP_COLUMN_1].isin(varstrings)]
 	return df[df[Constants.TARGET_CASE_IDX].isin(vdf[Constants.TARGET_CASE_IDX])]
@@ -27,7 +27,7 @@ def get_variants(df, max_des_vars_num=sys.maxsize, return_list=False):
 		vdf = vdf[vdf[Constants.TEMP_COLUMN_1].isin(nlarg_vars)]
 		vars_count = vdf[Constants.TEMP_COLUMN_1].value_counts()
 	res = vars_count.to_pandas().to_dict()
-	res = {",".join([activities[int(i)] for i in key.split(",")]): res[key] for key in res.keys()}
+	res = {Constants.VARIANTS_SEP.join([activities[int(i)] for i in key.split(Constants.VARIANTS_SEP)]): res[key] for key in res.keys()}
 	if return_list:
 		res = [{"variant": x, "count": y} for x, y in res.items()]
 	return res
